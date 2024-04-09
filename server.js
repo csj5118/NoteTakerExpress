cc7c5a9199a6e9caeb7a74f6f9080b95a04890fa
CJ <csh.joo77@gmail.com>
2024-04-09 17:59:22 -0400

    added pathing and changed the server.js in the root folder based off stack flow recommendation

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
-
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 app.use(express.static(path.join(__dirname, 'public')));

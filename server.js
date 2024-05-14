b808f2ebcbd7905226ee8754d29e48b49f3366ae
CJ <csh.joo77@gmail.com>
2024-05-13 20:20:04 -0400

    changed index.js and server.js to save notes and generate unique id's

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +14,7 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -22,15 +22,57 @@ app.get('*', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
   fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
     res.json(JSON.parse(data));
   });
 });
 
-app.post('/api/notes', (req, res) => {
 
+app.post('/api/notes', (req, res) => {
+  
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    
+    const notes = JSON.parse(data);
+
+    
+    const newNoteId = generateUniqueId();
+
+    
+    const newNote = {
+      id: newNoteId,
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    
+    notes.push(newNote);
+
+    
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(newNote); 
+    });
+  });
 });
 
+
+function generateUniqueId() {
+
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
